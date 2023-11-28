685229aa8dc961823ede13ddf6194422a4608809
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-11-29 11:06:54 +1300

    Refactoring

--- src/battleship.js
+++ src/battleship.js
@@ -4,7 +4,7 @@ import {showBoard} from '../DOM_module/board.DOM.js';
 import aiBoard from "../DOM_module/ai_board.DOM.js";
 import './battleship.css';
 
-const size = 10;
+const size = 5;
 const body = document.querySelector("body");
 const header = document.createElement("header");
 const main = document.createElement("main");
@@ -13,4 +13,4 @@ const player = gameboardFactory(size, size);
 const computer = initialize_computer_AI(size, size);
 
 body.append(header, main, footer);
-main.append(showBoard(player, size, size), aiBoard(computer, size, size));
+main.append(showBoard(player, size, size), aiBoard(computer, size, size, player));
