4ca5ab3539ed4eea32792adde5b2b1658efcd19c
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-12-09 19:28:02 +1300

    Refactoring

--- src/battleship.js
+++ src/battleship.js
@@ -4,7 +4,7 @@ import {showBoard} from '../DOM_module/board.DOM.js';
 import aiBoard from "../DOM_module/ai_board.DOM.js";
 import './battleship.css';
 
-const size = 6;
+const size = 10;
 const body = document.querySelector("body");
 const header = document.createElement("header");
 const main = document.createElement("main");
